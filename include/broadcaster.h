bce256908b749adb3b6b701d8b277316b41e4af0
SanczoPL <grzegorz600425@gmail.com>
2020-08-10 11:50:51 +0200

    Add sources

---
+++ include/broadcaster.h
@@ -0,0 +1,46 @@
+#ifndef BROADCASTER_H
+#define BROADCASTER_H
+
+#include <QDebug>
+#include <QJsonArray>
+#include <QJsonObject>
+#include <QObject>
+#include <QVector>
+#include <stdio.h>
+
+#include <mqt.h>
+#include <utils.h>
+
+class Broadcaster : public QObject {
+  Q_OBJECT
+
+public:
+  Broadcaster(QJsonObject const &a_config);
+  ~Broadcaster();
+
+private:
+  MQt m_IO{};
+
+public slots:
+  void configure(const QJsonObject &a_config);
+  void onConnect();
+  void onSubscribe(const qint32 topic);
+  void onUnsubscribe(const qint32 topic);
+  void onNewMessage(const QByteArray a_message);
+  void onSendCommand(const qint32 topic, const QJsonObject json);
+  void onConnected();
+  void onDisconnected();
+
+signals:
+  void subscribeRequest(QVector<qint32> const a_topics);
+  void unsubscribeRequest(QVector<qint32> const a_topics);
+  void sendMessageRequest(QByteArray const a_message);
+  void newMessage(QJsonObject const &a_json);
+  void connected();
+  void disconnected();
+
+private:
+  QString m_ip{};
+  quint16 m_port{};
+};
+#endif // BROADCASTER_H
---
+++ src/broadcaster.cc
@@ -0,0 +1,89 @@
+#include "broadcaster.h"
+
+auto constexpr MY_ID{3};
+
+Broadcaster::Broadcaster(QJsonObject const &a_config) {
+  H_Logger->trace("Broadcaster::Broadcaster()");
+  configure(a_config);
+}
+
+Broadcaster::~Broadcaster() {}
+
+void Broadcaster::configure(QJsonObject const &a_config) {
+  H_Logger->trace("Broadcaster::configure()");
+  QObject::connect(this, &Broadcaster::subscribeRequest, &m_IO,
+                   &MQt::onSubscribe);
+  QObject::connect(this, &Broadcaster::unsubscribeRequest, &m_IO,
+                   &MQt::onUnsubscribe);
+  QObject::connect(&m_IO, &MQt::newMessage, this, &Broadcaster::onNewMessage);
+  QObject::connect(this, &Broadcaster::sendMessageRequest, &m_IO,
+                   &MQt::onSendMessage);
+  QObject::connect(&m_IO, &MQt::connected, this, &Broadcaster::onConnected);
+  QObject::connect(&m_IO, &MQt::disconnected, this,
+                   &Broadcaster::onDisconnected);
+
+  m_ip = (a_config[IP].toString());
+  m_port = (static_cast<quint16>(a_config[PORT].toInt()));
+  onConnect();
+  H_Logger->trace("Broadcaster::configure() ip:{} port:{}", m_ip.toStdString(),
+                  QString::number(m_port).toStdString());
+}
+
+void Broadcaster::onConnect() { m_IO.startConnection(m_ip, m_port); }
+
+void Broadcaster::onSubscribe(qint32 topic) {
+  H_Logger->debug("Broadcaster::onSubscribe() topic:{}", topic);
+  QVector<qint32> topics{};
+  topics.push_back(topic);
+  emit(subscribeRequest(topics));
+}
+
+void Broadcaster::onUnsubscribe(qint32 topic) {
+  H_Logger->debug("Broadcaster::onUnsubscribe() topic:{}", topic);
+  QVector<qint32> topics{};
+  topics.push_back(topic);
+  emit(unsubscribeRequest(topics));
+}
+
+void Broadcaster::onNewMessage(QByteArray const a_message) {
+  Message message{};
+  message.parse(a_message);
+  QJsonObject jOut{{"none", "none"}};
+  bool ret = message.parse(a_message);
+  if (ret <= 0) {
+    H_Logger->warn("Broadcaster::onNewMessage() msg not correct");
+  } else {
+    Message::Header m_header = message.header();
+
+    H_Logger->trace(
+        "Broadcaster::onNewMessage() a_message.size():{} m_header.size:{}",
+        a_message.size() - 20, m_header.size);
+    if (message.isValid()) {
+      auto jDoc{QJsonDocument::fromJson(message.content())};
+      if (!jDoc.isObject()) {
+        H_Logger->trace("Broadcaster::onNewMessage() Recived invalid Message");
+      }
+
+      jOut = jDoc.object()[COMMAND].toObject();
+    }
+  }
+  emit(newMessage(jOut));
+  QByteArray stateData{QJsonDocument{jOut}.toJson(QJsonDocument::Compact)};
+  Message msg{};
+  msg.fromData(stateData, Message::JSON, MY_ID, 1);
+  H_Logger->trace("Broadcaster::onNewMessage from:{} \n{}", message.topic(),
+                  stateData.toStdString().c_str());
+}
+
+void Broadcaster::onSendCommand(const qint32 topic, const QJsonObject json) {
+  Message msg{};
+  QByteArray stateData{QJsonDocument{json}.toJson(QJsonDocument::Compact)};
+  msg.fromData(stateData, Message::JSON, MY_ID, topic);
+  H_Logger->trace("Broadcaster::onSendCommand to:{} \n{}", topic,
+                  stateData.toStdString().c_str());
+  emit(sendMessageRequest(msg.rawData()));
+}
+
+void Broadcaster::onConnected() { emit(connected()); }
+
+void Broadcaster::onDisconnected() { emit(disconnected()); }
