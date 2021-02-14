a8e6205b690dbd4ef712a85d8ba7daab94ff53c4
Grzegorz Matczak <grzegorz.matczak@unikie.com>
2021-02-14 18:49:43 +0100

    Add send ping with id

--- src/broadcaster.cc
+++ src/broadcaster.cc
@@ -10,6 +10,11 @@ constexpr auto PING_PONG{ "PingPong" };
 constexpr auto MESSAGE_TYPE{ "MessageType" };
 constexpr auto TIME{ "Time" };
 constexpr auto ERROR_DATA{ "Error" };
+constexpr auto SENDER{ "Sender" };
+constexpr auto FROM{ "From" };
+constexpr auto TO{ "To" };
+
+
 
 Broadcaster::Broadcaster(QJsonObject const& a_config) 
 	: m_id{ a_config[ID].toInt() } 
@@ -115,7 +120,7 @@ void Broadcaster::onNewMessage(QByteArray const a_message) {
 				else {
 					emit(newMessage(jOut));
 					QByteArray stateData{ QJsonDocument{jOut}.toJson(QJsonDocument::Compact) };
-					Logger->trace("Broadcaster::onNewMessage from:{} \n{}", message.topic(), stateData.toStdString().c_str());
+					Logger->trace("Broadcaster::onNewMessage from:{} to :{} \n message:{}", message.sender(), message.topic(), stateData.toStdString().c_str());
 				}
 			}
 		}
@@ -123,10 +128,11 @@ void Broadcaster::onNewMessage(QByteArray const a_message) {
 }
 
 void Broadcaster::onSendCommand(const qint32 topic, const QJsonObject json) {
+	QJsonObject cmd = { {COMMAND, json} };
 	MQtMessage msg{};
-	QByteArray stateData{ QJsonDocument{json}.toJson(QJsonDocument::Compact) };
+	QByteArray stateData{ QJsonDocument{cmd}.toJson(QJsonDocument::Compact) };
 	msg.fromData(stateData, MQtMessage::JSON, m_id, topic);
-	Logger->trace("Broadcaster::onSendCommand to:{} \n{}", topic, stateData.toStdString().c_str());
+	Logger->trace("Broadcaster::onSendCommand from {}  to:{} \nmessage:{}", m_id, topic, stateData.toStdString().c_str());
 	emit(sendMessageRequest(msg.rawData()));
 }
 
@@ -139,7 +145,18 @@ void Broadcaster::onSendImage(const qint32 topic, QByteArray image) {
 
 void Broadcaster::onSendPing(const qint32 topic) {
 	qint32 now = qint32(QDateTime::currentMSecsSinceEpoch());
-	QJsonObject json = { {MESSAGE_TYPE, PING}, {TIME, now} , {ID, m_id} };
+	QJsonObject json = { {MESSAGE_TYPE, PING}, {TIME, now} , {FROM, m_id}, {TO, topic} };
+	QJsonObject cmd = { {COMMAND, json} };
+	MQtMessage msg{};
+	QByteArray stateData{ QJsonDocument{cmd}.toJson(QJsonDocument::Compact) };
+	msg.fromData(stateData, MQtMessage::JSON, m_id, topic);
+	Logger->trace("Broadcaster::onSendPing() from {} to:{}", m_id, topic);
+	emit(sendMessageRequest(msg.rawData()));
+}
+
+void Broadcaster::onSendPingWithId(const qint32 topic, const qint32 id) {
+	qint32 now = qint32(QDateTime::currentMSecsSinceEpoch());
+	QJsonObject json = { {MESSAGE_TYPE, PING}, {TIME, now} , {FROM, m_id}, {TO, topic}, {ID,id} };
 	QJsonObject cmd = { {COMMAND, json} };
 	MQtMessage msg{};
 	QByteArray stateData{ QJsonDocument{cmd}.toJson(QJsonDocument::Compact) };
@@ -150,12 +167,13 @@ void Broadcaster::onSendPing(const qint32 topic) {
 
 void Broadcaster::onSendPingPong(QJsonObject json) {
 	json[MESSAGE_TYPE] = PING_PONG;
-	qint32 topic = json[ID].toInt();
+	qint32 from = json[TO].toInt();
+	qint32 to = json[FROM].toInt();
 	QJsonObject cmd = { {COMMAND, json} };
 	MQtMessage msg{};
 	QByteArray stateData{ QJsonDocument{cmd}.toJson(QJsonDocument::Compact) };
-	msg.fromData(stateData, MQtMessage::JSON, m_id, topic);
-	Logger->trace("Broadcaster::onSendPingPong() from {} to:{}", m_id, topic);
+	msg.fromData(stateData, MQtMessage::JSON, from, to);
+	Logger->trace("Broadcaster::onSendPingPong() from {} to:{}", from, to);
 	emit(sendMessageRequest(msg.rawData()));
 }
 
